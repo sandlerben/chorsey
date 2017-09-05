ff0798ea7e2d9c11548309010f95600728d8d5e8
Ben Sandler <ben1sandler@gmail.com>
2017-09-04 23:20:26 -0400

    handle verify get request

--- app.py
+++ app.py
@@ -8,7 +8,15 @@ def health():
 
 @app.route('/messages_callback')
 def messages_callback():
+    if request.method == 'GET':
+        mode = request.args.get('hub.mode')
+        verify_token = request.args.get('hub.verify_token')
+        if mode == 'subscribe' and verify_token == os.environ['facebook_secret']:
+            return request.args.get('hub.challenge')
+        else:
+            return 'Failed challenge', 403
     content = request.get_json()
+    print(content)
     return 'OK ;)'
 
 if __name__ == '__main__':
