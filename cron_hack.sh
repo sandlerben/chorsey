31fc066ada94e7c04aa6603988651a67ee60fd03
Ben Sandler <ben1sandler@gmail.com>
2017-09-08 22:37:02 -0400

    script runs on saturday

--- cron_hack.sh
+++ cron_hack.sh
@@ -2,4 +2,4 @@
 
 # This is a total hack based on the limitations of the heroku scheduler.
 # See https://stackoverflow.com/questions/9835095/how-can-i-schedule-a-weekly-job-on-heroku/18565294#18565294
-if [ "$(date +%u)" = 1 ]; then python cron.py; fi
+if [ "$(date +%u)" = 6 ]; then python cron.py; fi
